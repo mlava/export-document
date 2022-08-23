a6b9f5c5966fb5dae555346cb1d4d53997a96b8b
Mark Lavercombe <mlava@users.noreply.github.com>
2022-08-23 17:02:55 +1000

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'extension.js',
+        path: __dirname,
+        library: {
+            type: "module",
+        }
+    },
+    experiments: {
+        outputModule: true,
+    },
+	resolve: {
+        fallback: {
+            "fs": false
+        },
+    }
+};
